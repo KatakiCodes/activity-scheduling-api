7f6bd32f98636ab0d8f731dbb96f0b2457bdae89
Nelson Dos Santos <katakicode001@gmail.com>
2026-01-14 12:56:41 +0100

    feat (Application) : Adiciona a condição verificadora para cancelar e completar atividades

--- src/activity-scheduling.application/Commands/CancelActivity/CancelActivityHandler.cs
+++ src/activity-scheduling.application/Commands/CancelActivity/CancelActivityHandler.cs
@@ -1,4 +1,5 @@
 using activity_scheduling.application.Enums;
+using activity_scheduling.domain.Enums;
 using activity_scheduling.domain.Repositories.Contracts;
 using MediatR;
 
@@ -11,8 +12,10 @@ namespace activity_scheduling.application.Commands.CancelActivity
         public async Task<GenericCommandResult> Handle(CancelActivityCommand request, CancellationToken cancellationToken)
         {
             var activity = await _ActivityRepository.GetActivityByIdAsync(request.Id);
-            if(activity == null)
+            if (activity == null)
                 return new GenericCommandResult(false, "Atividade não encontrada", null, EStatusCodes.NOTFOUND);
+            else if (activity.State == EActivityState.COMPLETED)
+                return new GenericCommandResult(false, "Esta atividade já foi concluida", null, EStatusCodes.BADREQUEST);
             activity.CancelActivity();
             await _ActivityRepository.UpdateActivityAsync(activity);
             return new GenericCommandResult(true, "Atividade cancelada com sucesso", activity, EStatusCodes.OK);
--- src/activity-scheduling.application/Commands/CompleteActivity/CompleteActivityHandler.cs
+++ src/activity-scheduling.application/Commands/CompleteActivity/CompleteActivityHandler.cs
@@ -1,4 +1,5 @@
 using activity_scheduling.application.Enums;
+using activity_scheduling.domain.Enums;
 using activity_scheduling.domain.Repositories.Contracts;
 using MediatR;
 
@@ -13,6 +14,8 @@ namespace activity_scheduling.application.Commands.CompleteActivity
             var activity = await _ActivityRepository.GetActivityByIdAsync(request.Id);
             if(activity == null)
                 return new GenericCommandResult(false, "Atividade não encontrada", null, EStatusCodes.NOTFOUND);
+            else if(activity.State == EActivityState.COMPLETED)
+                return new GenericCommandResult(false, "Esta atividade já foi concluida", null, EStatusCodes.BADREQUEST);
             activity.CompleteActivity();
             await _ActivityRepository.UpdateActivityAsync(activity);
             return new GenericCommandResult(true, "Atividade concluida com sucesso", activity, EStatusCodes.OK);
